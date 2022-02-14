8788499a25fba09e1ffeeae72da7ae98bdea8a84
Sandip Chaudhary <sandip.choudhary@crystalsolution.com.np>
2022-02-15 00:58:30 +0545

    add support for dark mode and light mode

--- SettingPage.cs
+++ SettingPage.cs
@@ -39,11 +39,13 @@ namespace BatteryNotifier
         private void SettingPage_Load(object sender, EventArgs e)
         {
             LoadSettings();
+            ApplyTheme();
         }
 
         private void LoadSettings()
         {
             ShowAsWindow.Checked = appSetting.Default.showAsModal;
+            DarkModeCheckbox.Checked = appSetting.Default.darkThemeApplied;
 
             showFullBatteryNotification.Checked = appSetting.Default.fullBatteryNotification;
             fullBatteryTrackbar.Value = appSetting.Default.fullBatteryNotificationValue;
@@ -127,6 +129,7 @@ namespace BatteryNotifier
         {
             LoadSettings();
             SetDefaultLocation();
+            ApplyTheme();
         }
 
         private void AppHeaderTitle_MouseDown(object sender, MouseEventArgs e)
@@ -157,6 +160,80 @@ namespace BatteryNotifier
             if (appSetting.Default.showAsModal)
             {
                 mouseDown = false;
+            }
+        }
+
+        private void DarkModeCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            appSetting.Default.darkThemeApplied = DarkModeCheckbox.Checked;
+            appSetting.Default.Save();
+
+            ApplyTheme();
+        }
+
+        private void ApplyTheme()
+        {
+            if(appSetting.Default.darkThemeApplied)
+            {
+                ShowAsWindowPanel.BackColor = Color.FromArgb(20, 20, 20);
+                DarkModelPanel.BackColor = Color.FromArgb(20, 20, 20);
+                SettingHeader.BackColor = Color.Black;
+                AppHeaderTitle.ForeColor = Color.White;
+                SettingContainer.BackColor = Color.FromArgb(30, 30, 30);
+                FullBatteryNotificationGroupBox.ForeColor = Color.White;
+                ShowFullBatteryNotificationLabel.ForeColor = Color.White;
+
+                ShowAsWindowLabel.ForeColor = Color.White;
+                DarkModeLabel.ForeColor = Color.White;
+
+                fullBatteryTrackbar.BackColor = Color.FromArgb(30, 30, 30);
+                lowBatteryTrackbar.BackColor = Color.FromArgb(30, 30, 30);
+
+                BatteryPercentageLabel.ForeColor = Color.White;
+                FullBatterySoundLabel.ForeColor = Color.White;
+                fullbatterySoundPath.BackColor = Color.FromArgb(20, 20, 20);
+                fullbatterySoundPath.ForeColor = Color.White;
+                browseFullBatterySoundButton.BackColor = Color.FromArgb(20, 20, 20);
+                browseFullBatterySoundButton.ForeColor = Color.White;
+                LowBatteryNotificationLabel.ForeColor = Color.White;
+                LowBatteryPercentageLabel.ForeColor = Color.White;
+                LowBatterySoundLabel.ForeColor = Color.White;
+                lowBatterySoundPath.BackColor = Color.FromArgb(20, 20, 20);
+                lowBatterySoundPath.ForeColor = Color.White;
+                browseLowBatterySoundButton.BackColor = Color.FromArgb(20, 20, 20);
+                browseLowBatterySoundButton.ForeColor = Color.White;
+            }
+            else
+            {
+                ShowAsWindowPanel.BackColor = Color.WhiteSmoke;
+                DarkModelPanel.BackColor = Color.WhiteSmoke;
+                SettingHeader.BackColor = Color.AliceBlue;
+                AppHeaderTitle.ForeColor = Color.Black;
+                SettingContainer.BackColor = Color.White;
+
+                FullBatteryNotificationGroupBox.ForeColor = Color.Black;
+                ShowFullBatteryNotificationLabel.ForeColor = Color.Black;
+
+                fullBatteryTrackbar.BackColor = Color.White;
+                lowBatteryTrackbar.BackColor = Color.White;
+
+                ShowAsWindowLabel.ForeColor = Color.Black;
+                DarkModeLabel.ForeColor = Color.Black;
+
+                BatteryPercentageLabel.ForeColor = Color.Black;
+                FullBatterySoundLabel.ForeColor = Color.Black;
+                fullbatterySoundPath.BackColor= Color.White;
+                fullbatterySoundPath.ForeColor = Color.Black;
+                browseFullBatterySoundButton.BackColor = Color.LightGray;
+                browseFullBatterySoundButton.ForeColor = Color.Black;
+                LowBatteryNotificationLabel.ForeColor = Color.Black;
+                LowBatteryPercentageLabel.ForeColor= Color.Black;
+                LowBatterySoundLabel.ForeColor = Color.Black;
+                lowBatterySoundPath.BackColor = Color.White;
+                lowBatterySoundPath.ForeColor = Color.Black;
+                browseLowBatterySoundButton.BackColor = Color.LightGray;
+                browseLowBatterySoundButton.ForeColor = Color.Black;
+
             }
         }
     }
