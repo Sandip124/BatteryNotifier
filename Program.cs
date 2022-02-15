3661994eaaa01948e7594ccc2bf09eec4d38998e
Sandip Chaudhary <sandip.choudhary@crystalsolution.com.np>
2022-02-15 19:44:12 +0545

    fix multiple instance creation using mutex

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.VisualBasic;
 using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace BatteryNotifier
 {
     internal static class Program
     {
+        private static string appGuid = "D2ED1949-C00C-4F99-87DD-B5A6CE56A733";
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -13,9 +18,19 @@ namespace BatteryNotifier
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
+            using Mutex mutex = new Mutex(false, "Global\\" + appGuid);
+            if (!mutex.WaitOne(0, false))
+            {
+                Process[] proc = Process.GetProcessesByName("BatteryNotifier");
+                Interaction.AppActivate(proc[0].MainWindowTitle);
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Dashboard());
+
         }
     }
 }
