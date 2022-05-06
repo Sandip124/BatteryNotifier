8bd8620fd7e30bc5db6345a3feb75a5dae994750
Sandip Chaudhary <sandip.choudhary@crystalsolution.com.np>
2022-05-06 08:21:36 +0545

    chore: add squirrel for auto update on release

---
+++ Helpers/ProcessHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatteryNotifier.Helpers
+{
+    internal class ProcessHelper
+    {
+        public static bool IsAlreadyRunning()
+        {
+            return Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1;
+        }
+    }
+}
