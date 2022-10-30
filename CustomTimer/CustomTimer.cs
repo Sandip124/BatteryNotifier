64d57cd76de6fb363b28c5bba06047aa0171db23
Pankaj Chaudhary <67068794+Pnkj124@users.noreply.github.com>
2022-10-30 16:26:50 +0545

    remove unused namespaces

--- CustomTimer/CustomTimer.cs
+++ CustomTimer/CustomTimer.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace BatteryNotifier.CustomTimer
 {
     public partial class CustomTimer
